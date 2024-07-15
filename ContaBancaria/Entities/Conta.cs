27bf4cece5a6a9527203ba895e5303975d6b25aa
LorenaCamila <lorenacamila19@gmail.com>
2024-07-15 15:52:41 -0300

    Update

---
+++ ContaBancaria/Entities/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria.Entities
+{
+    internal class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta()
+        {
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+    }
+}
---
+++ ContaBancaria/Entities/ContaEmpresarial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria.Entities
+{
+    internal class ContaEmpresarial : Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaEmpresarial()
+        {
+        }
+
+        public ContaEmpresarial(int numero, string titular, double saldo, double limiteEmprestimo) : base(numero, titular, saldo)
+        {
+            LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public void Emprestimo(double valor)
+        {
+            if(valor <= LimiteEmprestimo)
+            {
+                Saldo += valor;
+            }
+        }
+    }
+}
